8deda117f22767e7dc319fc16250156aad234f48
Alex Stokes <r.alex.stokes@gmail.com>
2021-03-28 12:22:38 -0700

    use rustyline for std io repl and sketch namespace prompt

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,8 @@
 use crate::reader::Form;
+use std::cmp;
 use std::default::Default;
 use std::fmt;
+use std::rc::Rc;
 use thiserror::Error;
 
 #[derive(Debug)]
@@ -18,33 +20,69 @@ pub enum EvaluationError {
     Unknown,
 }
 
-pub struct Namespace {
+type Namespace = Rc<NamespaceInner>;
+
+#[derive(Debug)]
+pub struct NamespaceInner {
     name: String,
 }
 
-impl fmt::Display for Namespace {
+impl cmp::PartialEq for NamespaceInner {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl fmt::Display for NamespaceInner {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.name)
     }
 }
 
-impl Default for Namespace {
+impl Default for NamespaceInner {
     fn default() -> Self {
-        Namespace {
+        NamespaceInner {
             name: "sigil".to_string(),
         }
     }
 }
 
-#[derive(Default)]
+#[derive(Debug)]
 pub struct Interpreter {
-    pub current_namespace: Namespace,
+    // index into `namespaces`
+    current_namespace: usize,
+    namespaces: Vec<Namespace>,
     _env: Environment,
 }
 
+impl Default for Interpreter {
+    fn default() -> Self {
+        let namespaces = vec![Rc::new(NamespaceInner::default())];
+        Interpreter {
+            current_namespace: 0,
+            namespaces: namespaces,
+            _env: Environment::default(),
+        }
+    }
+}
+
 impl Interpreter {
+    pub fn current_namespace(&self) -> Namespace {
+        self.namespaces[self.current_namespace].clone()
+    }
+
     pub fn evaluate<'a>(&mut self, form: Form<'a>) -> Result<Form<'a>, EvaluationError> {
+        use Form::*;
+
         let result = match form {
+            Number(n) => {
+                // demo name space switch in repl
+                self.namespaces.push(Rc::new(NamespaceInner {
+                    name: n.to_string(),
+                }));
+                self.current_namespace += 1;
+                Number(n)
+            }
             other @ _ => other,
         };
         Ok(result)
--- src/repl.rs
+++ src/repl.rs
@@ -1,7 +1,10 @@
 use crate::interpreter::Interpreter;
 use crate::reader::read;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use std::default::Default;
-use std::io::{self, BufRead, Write};
+use std::error::Error;
+use std::fmt::Write;
 
 #[derive(Default)]
 pub struct StdRepl {
@@ -9,34 +12,50 @@ pub struct StdRepl {
 }
 
 impl StdRepl {
-    pub fn run(&mut self) -> io::Result<()> {
-        let stdin = io::stdin();
-        let reader = stdin.lock();
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        let mut editor = Editor::<()>::new();
+        let mut prompt_buffer = String::new();
 
-        let mut current_namespace = &self.interpreter.current_namespace;
-
-        print!("{}=> ", current_namespace);
-        io::stdout().flush()?;
-        for line in reader.lines() {
-            let line = line.unwrap();
-            let forms = match read(&line) {
-                Ok(forms) => forms,
-                Err(e) => {
-                    println!("{}", e);
-                    continue;
-                }
-            };
-            for form in forms {
-                match self.interpreter.evaluate(form) {
-                    Ok(result) => println!("{}", result),
-                    Err(e) => {
-                        println!("{}", e);
+        let mut current_namespace = self.interpreter.current_namespace();
+        write!(prompt_buffer, "{}=> ", current_namespace)?;
+        loop {
+            let next_line = editor.readline(&prompt_buffer);
+            match next_line {
+                Ok(line) => {
+                    let forms = match read(&line) {
+                        Ok(forms) => forms,
+                        Err(e) => {
+                            println!("{}", e);
+                            continue;
+                        }
+                    };
+                    for form in forms {
+                        match self.interpreter.evaluate(form) {
+                            Ok(result) => println!("{}", result),
+                            Err(e) => {
+                                println!("{}", e);
+                            }
+                        }
+                    }
+                    if self.interpreter.current_namespace() != current_namespace {
+                        current_namespace = self.interpreter.current_namespace();
+                        prompt_buffer.clear();
+                        write!(prompt_buffer, "{}=> ", current_namespace)?;
                     }
                 }
+                Err(ReadlineError::Interrupted) => {
+                    println!("^C");
+                    break;
+                }
+                Err(ReadlineError::Eof) => {
+                    println!("^D");
+                    break;
+                }
+                Err(err) => {
+                    println!("error: {:?}", err);
+                    break;
+                }
             }
-            current_namespace = &self.interpreter.current_namespace;
-            print!("{}=> ", current_namespace);
-            io::stdout().flush()?;
         }
         Ok(())
     }
