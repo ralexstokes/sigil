8066af6fa0b14ecd9467ff1e5481e0bebb91e961
Alex Stokes <r.alex.stokes@gmail.com>
2021-03-27 17:30:07 -0700

    read recursive types

--- src/reader.rs
+++ src/reader.rs
@@ -1,9 +1,11 @@
+use combine::error::ParseError;
 use combine::parser::{
     char::alpha_num, char::newline, char::char, char::digit, char::space, choice::choice, choice::or, choice::optional, combinator::from_str,
-    range::range, range::recognize, repeat::many, repeat::skip_many1, repeat::skip_until, token::eof,
+    range::range, range::recognize, repeat::many, repeat::skip_many1, repeat::skip_until, sequence::between, token::eof,
 };
-use combine::stream::position::Stream;
-use combine::{Parser, EasyParser};
+use combine::stream::position;
+use combine::{Parser, EasyParser, RangeStream};
+use combine::parser;
 use thiserror::Error;
 
 #[derive(Debug, PartialEq)]
@@ -18,6 +20,10 @@ pub enum Form<'a> {
     Symbol(&'a str, Option<&'a str>),
     Comment(&'a str),
     Whitespace(&'a str),
+    List(Vec<Form<'a>>),
+    Vector(Vec<Form<'a>>),
+    Map(Vec<Form<'a>>),
+    Set(Vec<Form<'a>>),
 }
 
 #[derive(Debug, Error)]
@@ -30,44 +36,71 @@ pub enum ReaderError {
     Unknown,
 }
 
+#[inline]
+fn read_forms<'a, Input>() -> impl Parser<Input, Output=Vec<Form<'a>>>
+where
+    Input: RangeStream<Token = char, Range = &'a str> + 'a,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position> 
+{
+    read_forms_inner()
+}
+
+parser! {
+    #[inline]
+    fn read_forms_inner['a, Input]()(Input) -> Vec<Form<'a>>
+    where [ Input: RangeStream<Token = char, Range = &'a str> + 'a]
+    {
+        let number = from_str(recognize(skip_many1(digit()))).map(Form::Number);
+        let nil = recognize(range("nil")).map(|_| Form::Nil);
+        let true_parser = recognize(range("true")).map(|_| Form::Bool(true));
+        let false_parser = recognize(range("false")).map(|_| Form::Bool(false));
+        let whitespace = recognize(skip_many1(or(space(), char(',')))).map(Form::Whitespace);
+
+        let string =
+            (char('"'), recognize(skip_until(char('"'))), char('"')).map(|(_, s, _)| Form::String(s));
+        let identifier_with_namespace = || (recognize(skip_many1(alpha_num())), optional((char('/'), recognize(skip_many1(alpha_num()))))).map(|(first, rest)| {
+            if let Some((_, rest)) = rest {
+                (rest, Some(first))
+            } else {
+                (first, None)
+            }
+        });
+        let symbol = identifier_with_namespace().map(|(id, ns)| Form::Symbol(id, ns));
+        let keyword = (char(':'), identifier_with_namespace()).map(|(_, (id, ns))| Form::Keyword(id, ns));
+        let comment = (char(';'), recognize(skip_until(or(eof(), newline().map(|_| ())))), optional(newline())).map(|(_, s, _)| Form::Comment(s));
+        let list = between(char('('), char(')'), read_forms()).map(Form::List);
+        let vector = between(char('['), char(']'), read_forms()).map(Form::Vector);
+        let map = between(char('{'), char('}'), read_forms()).map(Form::Map);
+
+        let forms = choice((
+            whitespace,
+            number,
+            nil,
+            true_parser,
+            false_parser,
+            string,
+            keyword,
+            symbol,
+            comment,
+            list,
+            vector,
+            map,
+        ));
+
+        many::<Vec<_>, _, _>(forms)
+    }
+}
+
 pub fn read(input: &str) -> Result<Vec<Form>, ReaderError> {
-    let number = from_str(recognize(skip_many1(digit()))).map(Form::Number);
-    let nil = recognize(range("nil")).map(|_| Form::Nil);
-    let true_parser = recognize(range("true")).map(|_| Form::Bool(true));
-    let false_parser = recognize(range("false")).map(|_| Form::Bool(false));
-    let whitespace = recognize(skip_many1(or(space(), char(',')))).map(Form::Whitespace);
-
-    let string =
-        (char('"'), recognize(skip_until(char('"'))), char('"')).map(|(_, s, _)| Form::String(s));
-    let identifier_with_namespace = || (recognize(skip_many1(alpha_num())), optional((char('/'), recognize(skip_many1(alpha_num()))))).map(|(first, rest)| {
-        if let Some((_, rest)) = rest {
-            (rest, Some(first))
-        } else {
-            (first, None)
-        }
-    });
-    let symbol = identifier_with_namespace().map(|(id, ns)| Form::Symbol(id, ns));
-    let keyword = (char(':'), identifier_with_namespace()).map(|(_, (id, ns))| Form::Keyword(id, ns));
-    let comment = (char(';'), recognize(skip_until(or(eof(), newline().map(|_| ())))), optional(newline())).map(|(_, s, _)| Form::Comment(s));
-
-    let forms = choice((
-        whitespace,
-        number,
-        nil,
-        true_parser,
-        false_parser,
-        string,
-        keyword,
-        symbol,
-        comment
-    ));
-
-    let eof = recognize(eof()).map(|_| Vec::<Form<'_>>::new());
-    let mut parser = (many::<Vec<_>, _, _>(forms), eof);
-    let parse_result = parser.easy_parse(Stream::new(input));
+    let mut parser = (read_forms(), eof()).map(|(result, _)| result);
+    let parse_result = parser.easy_parse(position::Stream::new(input));
     match parse_result {
-        Ok(((forms, _), _)) => Ok(forms),
-        Err(error) => Err(ReaderError::ParserError(error.to_string())),
+        Ok((forms, _)) => {
+                Ok(forms)
+        },
+        Err(error) => {
+            Err(ReaderError::ParserError(error.to_string()))
+        },
     }
 }
 
@@ -103,6 +136,32 @@ mod tests {
             ("; sdlkfjsldfjsldjflsdjf", vec![Comment(" sdlkfjsldfjsldjflsdjf")]),
             ("foo/bar true ;; some comment", vec![Symbol("bar", Some("foo")), Whitespace(" "), Bool(true), Whitespace(" "), Comment("; some comment")]),
             ("baz ;; comment \nfoo12", vec![Symbol("baz", None), Whitespace(" "), Comment("; comment "), Symbol("foo12", None)]),
+            ("()", vec![List(vec!())]),
+            ("(a b c)", vec![List(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
+            ("(12 :foo/bar \"extra\")", vec![List(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
+            ("[]", vec![Vector(vec!())]),
+            ("[a b c]", vec![Vector(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
+            ("[12 :foo/bar \"extra\"]", vec![Vector(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
+            ("{}", vec![Map(vec!())]),
+            ("{a b c}", vec![Map(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
+            ("{12 :foo/bar \"extra\"}", vec![Map(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
+            ("() []", vec!(List(vec!()), Whitespace(" "), Vector(vec!()))),
+            ("(())", vec!(List(vec!(List(vec!()))))),
+            ("(([]))", vec!(List(vec!(List(vec!(Vector(vec!()))))))),
+            ("(([]))()", vec!(List(vec!(List(vec!(Vector(vec!()))))), List(vec!()))),
+            ("(12 (true [34 false]))(), (7)", vec!(
+                List(vec!(
+                    Number(12), 
+                    Whitespace(" "), 
+                    List(vec!(
+                        Bool(true), 
+                        Whitespace(" "),
+                        Vector(vec!(Number(34), 
+                                    Whitespace(" "), 
+                                    Bool(false))))))),
+                List(vec!()), 
+                Whitespace(", "), 
+                List(vec!(Number(7))))),
         ]; 
         for (input, expected) in cases {
             match read(input) {
