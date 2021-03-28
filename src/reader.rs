3dd4b724def99d47f8f71c0c31769f44fe7b81a1
Alex Stokes <r.alex.stokes@gmail.com>
2021-03-27 22:50:52 -0700

    cargo fmt fixup

--- src/reader.rs
+++ src/reader.rs
@@ -1,13 +1,15 @@
 use combine::error::ParseError;
-use combine::parser::{
-    char::alpha_num, char::newline, char::char, char::digit, char::space, choice::choice, choice::or, choice::optional, combinator::from_str,
-    range::range, range::recognize, repeat::many, repeat::skip_many1, repeat::skip_until, sequence::between, token::eof, token::one_of
-};
-use combine::stream::position;
-use combine::{Parser, EasyParser, RangeStream};
 use combine::parser;
-use thiserror::Error;
+use combine::parser::{
+    char::alpha_num, char::char, char::digit, char::newline, char::space, choice::choice,
+    choice::optional, choice::or, combinator::from_str, range::range, range::recognize,
+    repeat::many, repeat::skip_many1, repeat::skip_until, sequence::between, token::eof,
+    token::one_of,
+};
+use combine::stream::position::Stream;
+use combine::{EasyParser, Parser, RangeStream};
 use std::fmt;
+use thiserror::Error;
 
 #[derive(Debug, PartialEq)]
 pub enum Form<'a> {
@@ -16,7 +18,7 @@ pub enum Form<'a> {
     Number(u64),
     String(&'a str),
     // identifier, optional namespace
-    Keyword(&'a str, Option<&'a str> ),
+    Keyword(&'a str, Option<&'a str>),
     // identifier, optional namespace
     Symbol(&'a str, Option<&'a str>),
     Comment(&'a str),
@@ -26,7 +28,6 @@ pub enum Form<'a> {
     Map(Vec<Form<'a>>),
     Set(Vec<Form<'a>>),
 }
-
 
 impl fmt::Display for Form<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -43,13 +44,13 @@ impl fmt::Display for Form<'_> {
                     write!(f, "{}/", ns)?;
                 }
                 write!(f, "{}", id)
-            },
+            }
             Symbol(id, ns_opt) => {
                 if let Some(ns) = ns_opt {
                     write!(f, "{}/", ns)?;
                 }
                 write!(f, "{}", id)
-            },
+            }
             Comment(s) => write!(f, ";{}", s),
             Whitespace(s) => write!(f, "{}", s),
             List(elems) => {
@@ -58,28 +59,28 @@ impl fmt::Display for Form<'_> {
                     write!(f, "{}", elem)?;
                 }
                 write!(f, ")")
-            },
+            }
             Vector(elems) => {
                 write!(f, "[")?;
                 for elem in elems {
                     write!(f, "{}", elem)?;
                 }
                 write!(f, "]")
-            },
+            }
             Map(elems) => {
                 write!(f, "{{")?;
                 for elem in elems {
                     write!(f, "{}", elem)?;
                 }
                 write!(f, "}}")
-            },
+            }
             Set(elems) => {
                 write!(f, "#{{")?;
                 for elem in elems {
                     write!(f, "{}", elem)?;
                 }
                 write!(f, "}}")
-            },
+            }
         }
     }
 }
@@ -95,10 +96,10 @@ pub enum ReaderError {
 }
 
 #[inline]
-fn read_forms<'a, Input>() -> impl Parser<Input, Output=Vec<Form<'a>>>
+fn read_forms<'a, Input>() -> impl Parser<Input, Output = Vec<Form<'a>>>
 where
     Input: RangeStream<Token = char, Range = &'a str> + 'a,
-    Input::Error: ParseError<Input::Token, Input::Range, Input::Position> 
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     read_forms_inner()
 }
@@ -157,14 +158,10 @@ parser! {
 
 pub fn read(input: &str) -> Result<Vec<Form>, ReaderError> {
     let mut parser = (read_forms(), eof()).map(|(result, _)| result);
-    let parse_result = parser.easy_parse(position::Stream::new(input));
+    let parse_result = parser.easy_parse(Stream::new(input));
     match parse_result {
-        Ok((forms, _)) => {
-                Ok(forms)
-        },
-        Err(error) => {
-            Err(ReaderError::ParserError(error.to_string()))
-        },
+        Ok((forms, _)) => Ok(forms),
+        Err(error) => Err(ReaderError::ParserError(error.to_string())),
     }
 }
 
@@ -182,11 +179,23 @@ mod tests {
             (",,,", vec![Whitespace(",,,")]),
             ("  ", vec![Whitespace("  ")]),
             (" , ", vec![Whitespace(" , ")]),
-            ("true, , , false", vec![Bool(true), Whitespace(", , , "), Bool(false)]),
+            (
+                "true, , , false",
+                vec![Bool(true), Whitespace(", , , "), Bool(false)],
+            ),
             ("", vec![]),
             ("baz", vec![Symbol("baz", None)]),
             ("foo/baz", vec![Symbol("baz", Some("foo"))]),
-            ("foo/baz bar true", vec![Symbol("baz", Some("foo")), Whitespace(" "), Symbol("bar", None), Whitespace(" "), Bool(true)]),
+            (
+                "foo/baz bar true",
+                vec![
+                    Symbol("baz", Some("foo")),
+                    Whitespace(" "),
+                    Symbol("bar", None),
+                    Whitespace(" "),
+                    Bool(true),
+                ],
+            ),
             ("\"\"", vec![String("")]),
             ("\"hi\"", vec![String("hi")]),
             (
@@ -195,55 +204,170 @@ mod tests {
             ),
             (r#""abc""#, vec![String("abc")]),
             (":foobar", vec![Keyword("foobar", None)]),
-            (":net/hi", vec![Keyword("hi", Some("net") )]),
+            (":net/hi", vec![Keyword("hi", Some("net"))]),
             (":a0987234", vec![Keyword("a0987234", None)]),
-            ("; sdlkfjsldfjsldjflsdjf", vec![Comment(" sdlkfjsldfjsldjflsdjf")]),
-            ("foo/bar true ;; some comment", vec![Symbol("bar", Some("foo")), Whitespace(" "), Bool(true), Whitespace(" "), Comment("; some comment")]),
-            ("baz ;; comment \nfoo12", vec![Symbol("baz", None), Whitespace(" "), Comment("; comment "), Symbol("foo12", None)]),
-            ("()", vec![List(vec!())]),
-            ("(a b c)", vec![List(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
-            ("(12 :foo/bar \"extra\")", vec![List(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
-            ("[]", vec![Vector(vec!())]),
-            ("[a b c]", vec![Vector(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
-            ("[12 :foo/bar \"extra\"]", vec![Vector(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
-            ("{}", vec![Map(vec!())]),
-            ("{a b c}", vec![Map(vec!(Symbol("a", None), Whitespace(" "), Symbol("b", None), Whitespace(" "), Symbol("c", None)))]),
-            ("{12 :foo/bar \"extra\"}", vec![Map(vec!(Number(12), Whitespace(" "), Keyword("bar", Some("foo")), Whitespace(" "), String("extra")))]),
-            ("() []", vec!(List(vec!()), Whitespace(" "), Vector(vec!()))),
-            ("(())", vec!(List(vec!(List(vec!()))))),
-            ("(([]))", vec!(List(vec!(List(vec!(Vector(vec!()))))))),
-            ("(([]))()", vec!(List(vec!(List(vec!(Vector(vec!()))))), List(vec!()))),
-            ("(12 (true [34 false]))(), (7)", vec!(
-                List(vec!(
-                    Number(12), 
-                    Whitespace(" "), 
-                    List(vec!(
-                        Bool(true), 
+            (
+                "; sdlkfjsldfjsldjflsdjf",
+                vec![Comment(" sdlkfjsldfjsldjflsdjf")],
+            ),
+            (
+                "foo/bar true ;; some comment",
+                vec![
+                    Symbol("bar", Some("foo")),
+                    Whitespace(" "),
+                    Bool(true),
+                    Whitespace(" "),
+                    Comment("; some comment"),
+                ],
+            ),
+            (
+                "baz ;; comment \nfoo12",
+                vec![
+                    Symbol("baz", None),
+                    Whitespace(" "),
+                    Comment("; comment "),
+                    Symbol("foo12", None),
+                ],
+            ),
+            ("()", vec![List(vec![])]),
+            (
+                "(a b c)",
+                vec![List(vec![
+                    Symbol("a", None),
+                    Whitespace(" "),
+                    Symbol("b", None),
+                    Whitespace(" "),
+                    Symbol("c", None),
+                ])],
+            ),
+            (
+                "(12 :foo/bar \"extra\")",
+                vec![List(vec![
+                    Number(12),
+                    Whitespace(" "),
+                    Keyword("bar", Some("foo")),
+                    Whitespace(" "),
+                    String("extra"),
+                ])],
+            ),
+            ("[]", vec![Vector(vec![])]),
+            (
+                "[a b c]",
+                vec![Vector(vec![
+                    Symbol("a", None),
+                    Whitespace(" "),
+                    Symbol("b", None),
+                    Whitespace(" "),
+                    Symbol("c", None),
+                ])],
+            ),
+            (
+                "[12 :foo/bar \"extra\"]",
+                vec![Vector(vec![
+                    Number(12),
+                    Whitespace(" "),
+                    Keyword("bar", Some("foo")),
+                    Whitespace(" "),
+                    String("extra"),
+                ])],
+            ),
+            ("{}", vec![Map(vec![])]),
+            (
+                "{a b c}",
+                vec![Map(vec![
+                    Symbol("a", None),
+                    Whitespace(" "),
+                    Symbol("b", None),
+                    Whitespace(" "),
+                    Symbol("c", None),
+                ])],
+            ),
+            (
+                "{12 :foo/bar \"extra\"}",
+                vec![Map(vec![
+                    Number(12),
+                    Whitespace(" "),
+                    Keyword("bar", Some("foo")),
+                    Whitespace(" "),
+                    String("extra"),
+                ])],
+            ),
+            ("() []", vec![List(vec![]), Whitespace(" "), Vector(vec![])]),
+            ("(())", vec![List(vec![List(vec![])])]),
+            ("(([]))", vec![List(vec![List(vec![Vector(vec![])])])]),
+            (
+                "(([]))()",
+                vec![List(vec![List(vec![Vector(vec![])])]), List(vec![])],
+            ),
+            (
+                "(12 (true [34 false]))(), (7)",
+                vec![
+                    List(vec![
+                        Number(12),
                         Whitespace(" "),
-                        Vector(vec!(Number(34), 
-                                    Whitespace(" "), 
-                                    Bool(false))))))),
-                List(vec!()), 
-                Whitespace(", "), 
-                List(vec!(Number(7))))),
-            ("#{}", vec!(Set(vec!()))),
-            ("#{1 2 3}", vec!(Set(vec!(Number(1), Whitespace(" "), Number(2), Whitespace(" "), Number(3))))),
-            ("#{(1 3) :foo}", vec!(Set(vec!(List(vec!(Number(1), Whitespace(" "), Number(3))), Whitespace(" "), Keyword("foo", None))))),
-            ("+ ! =", vec!(Symbol("+", None), Whitespace(" "), Symbol("!", None), Whitespace(" "), Symbol("=", None))),
-            ("(defn foo [a] (+ a 1))", vec!(List(vec!(
-                Symbol("defn", None),
-                Whitespace(" "),
-                Symbol("foo", None),
-                Whitespace(" "),
-                Vector(vec!(Symbol("a", None))),
-                Whitespace(" "),
-                List(vec!(Symbol("+", None), Whitespace(" "), Symbol("a", None), Whitespace(" "), Number(1)))
-            )))),
-        ]; 
+                        List(vec![
+                            Bool(true),
+                            Whitespace(" "),
+                            Vector(vec![Number(34), Whitespace(" "), Bool(false)]),
+                        ]),
+                    ]),
+                    List(vec![]),
+                    Whitespace(", "),
+                    List(vec![Number(7)]),
+                ],
+            ),
+            ("#{}", vec![Set(vec![])]),
+            (
+                "#{1 2 3}",
+                vec![Set(vec![
+                    Number(1),
+                    Whitespace(" "),
+                    Number(2),
+                    Whitespace(" "),
+                    Number(3),
+                ])],
+            ),
+            (
+                "#{(1 3) :foo}",
+                vec![Set(vec![
+                    List(vec![Number(1), Whitespace(" "), Number(3)]),
+                    Whitespace(" "),
+                    Keyword("foo", None),
+                ])],
+            ),
+            (
+                "+ ! =",
+                vec![
+                    Symbol("+", None),
+                    Whitespace(" "),
+                    Symbol("!", None),
+                    Whitespace(" "),
+                    Symbol("=", None),
+                ],
+            ),
+            (
+                "(defn foo [a] (+ a 1))",
+                vec![List(vec![
+                    Symbol("defn", None),
+                    Whitespace(" "),
+                    Symbol("foo", None),
+                    Whitespace(" "),
+                    Vector(vec![Symbol("a", None)]),
+                    Whitespace(" "),
+                    List(vec![
+                        Symbol("+", None),
+                        Whitespace(" "),
+                        Symbol("a", None),
+                        Whitespace(" "),
+                        Number(1),
+                    ]),
+                ])],
+            ),
+        ];
         for (input, expected) in cases {
             match read(input) {
                 Ok(result) => assert_eq!(result, expected),
-                Err(e) =>  {
+                Err(e) => {
                     dbg!(e);
                     panic!();
                 }
