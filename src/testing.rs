aca5ef8b2da1de57ff581b2923c77c0a6fa2435a
Alex Stokes <r.alex.stokes@gmail.com>
2022-03-10 18:58:09 -0700

    quiet testing warnings

--- src/testing.rs
+++ src/testing.rs
@@ -7,23 +7,20 @@ pub fn run_eval_test(test_cases: &[(&str, RuntimeValue)]) {
     let mut has_err = false;
     for (input, expected) in test_cases {
         let mut interpreter = Interpreter::default();
-        let mut final_result: Option<RuntimeValue> = None;
         let original_scope_len = interpreter.scopes.len();
         assert!(original_scope_len == EXPECTED_STARTING_SCOPE_LEN);
         // assert!(interpreter.apply_stack.is_empty());
-        match interpreter.interpret(input) {
-            Ok(result) => {
-                final_result = Some(result.last().unwrap().clone());
-            }
+        let final_result = match interpreter.interpret(input) {
+            Ok(result) => Some(result.last().unwrap().clone()),
             Err(e) => {
                 has_err = true;
                 println!(
                     "failure: evaluating `{}`; should give `{}` but errored: {}",
                     input, expected, e
                 );
-                break;
+                None
             }
-        }
+        };
         if has_err {
             continue;
         }
